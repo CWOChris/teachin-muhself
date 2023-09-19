605f3e708f30c9bb9cd9543ff0619836143c18bb
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-19 02:12:45 -0500

    finished 65

---
+++ JS/65-quotes-in-a-string/js/script.js
@@ -0,0 +1,9 @@
+var title;
+var message;
+title = "Molly's Special Offers";
+message = '<a href="https://javascriptbook.com/code/c02/sale.html">25% off!</a>'
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;
