44f6775da2496446eb6eb7d28d9f87e41bbbf198
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-19 02:27:49 -0500

    finished 66

---
+++ JS/66-var-to-store-boolean/js/script.js
@@ -0,0 +1,13 @@
+var instock;
+var shipping;
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className = shipping
+
+// this thing is cool.  reference the style sheet from the book at https://javascriptbook.com/code/c02/boolean-variable.html 
+// in order to see what I mean. fantastic.
