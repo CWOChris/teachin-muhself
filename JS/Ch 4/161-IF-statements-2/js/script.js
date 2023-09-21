914749d0a8f282bd03168c645e7ea4ed7f9bb9bc
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-21 01:33:57 -0500

    finished 161

---
+++ JS/Ch 4/161-IF-statements-2/js/script.js
@@ -0,0 +1,13 @@
+var score = 75;
+var msg = '';
+
+function congratulate() {
+    msg += 'Congratulations! ';
+}
+
+if (score>=50) {
+    congratulate();
+    msg += 'Proceed to the next round.';
+}
+var el = document.getElementById('answer');
+el.innerHTML = msg;
