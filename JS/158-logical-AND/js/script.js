5d774926888b9bc0413059d738e3ad093996647f
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-21 01:06:29 -0500

    finished 158

--- JS/158-logical-AND/js/script.js
+++ JS/158-logical-AND/js/script.js
@@ -1,9 +1,11 @@
-var score1 = 90;
-var score2 = 95;
-var highScore1 = 75;
-var highScore2 = 95;
+var score1 = 8;
+var score2 = 8;
+var pass1 = 6;
+var pass2 = 6;
 
-var comparison = (score1 + score2) > (highScore1 + highScore2);
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
+
+var msg = 'Both rounds passed: ' + passBoth
 
 var el = document.getElementById('answer');
-el.textContent = 'New high score: ' + comparison;
+el.textContent = msg;
