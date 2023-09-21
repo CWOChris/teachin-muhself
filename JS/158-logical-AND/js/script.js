e7e28f119a1e876f163fdc124b281f24643458ce
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-21 01:12:29 -0500

    added 159, adjusted 158

--- JS/158-logical-AND/js/script.js
+++ JS/158-logical-AND/js/script.js
@@ -3,9 +3,9 @@ var score2 = 8;
 var pass1 = 6;
 var pass2 = 6;
 
-var passBoth = (score1 >= pass1) && (score2 >= pass2);
+var minPass = ((score1 >= pass1) || (score2 >= pass2));
 
-var msg = 'Both rounds passed: ' + passBoth
+var msg = 'Resit required: ' + !minPass;
 
-var el = document.getElementById('answer');
+var el = document.getElementById('Answer');
 el.textContent = msg;
