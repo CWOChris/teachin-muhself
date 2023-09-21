0d74f69904fcf8d47d2ba5b9fae440f4d2af0729
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-20 19:11:13 -0500

    added 125

---
+++ JS/125-BOM/js/script.js
@@ -0,0 +1,8 @@
+var msg = '<h2.browser window</h2><p>width: ' + window.innerWidth + '</p>';
+msg += '<p>height: ' + window.innerHeight + '</p>';
+msg += '<h2>history</h2><p>width: ' + window.history.length + '</p>';
+msg += '<h2>screen</h2><p>widthe: ' + window.screen.width + '</p>';
+msg += '<p>height: ' + window.screen.height + '</p>'
+var el = document.getElementById('info');
+el.innerHTML = msg;
+alert('Current page: ' + window.location);
