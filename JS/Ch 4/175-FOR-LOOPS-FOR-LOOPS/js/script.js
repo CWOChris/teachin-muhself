882d1532725228aeffeb7aa4d3b58f8efbe432ba
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-21 19:46:33 -0500

    first commit 21sep23

--- JS/Ch 4/175-FOR-LOOPS-FOR-LOOPS/js/script.js
+++ JS/Ch 4/175-FOR-LOOPS-FOR-LOOPS/js/script.js
@@ -5,9 +5,19 @@ var msg = '';
 var i;
 
 for (i = 0; i < arrayLength; i++) {
+    var score = scores[i]
     roundNumber = (i + 1);
     msg += 'Round ' + roundNumber + ': ';
     msg += scores[i] + '<br />';
 }
 
 document.getElementById('answer').innerHTML = msg;
+
+
+// This is what I discussed with the tutor. Go to the page and look at the console log //
+var randNum = Math.random() * scores.length;
+var randIndex = Math.floor(randNum);
+scores.forEach(function(score, i) {
+console.log("Score: ", score);
+console.log(i);
+})
