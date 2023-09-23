26728dd7e5f86fb51e86af0a223525b855c50c98
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 02:14:08 -0500

    finished 196-197

--- JS/Ch-5/196-197-selecting-by-class-and-tag/js/script.js
+++ JS/Ch-5/196-197-selecting-by-class-and-tag/js/script.js
@@ -4,3 +4,12 @@ if (elements.length > 2) {
     var el = elements[2];
     el.className = 'cool';
 }
+
+var elements = document.getElementsByTagName('li');
+
+if (elements > 0) {
+    var el = elements[0];
+    el.className = 'cool';
+}
+
+// second part of script doesn't function as intended --\o + o/--
