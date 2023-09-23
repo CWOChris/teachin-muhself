354943b015b5f84ccd12a4c1e5bfe7e01f22fc1a
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 14:23:48 -0500

    285 finished

--- JS/Ch-6/286-mutation-events/js/script.js
+++ JS/Ch-6/286-mutation-events/js/script.js
@@ -7,5 +7,24 @@ counter = document.getElementById('counter');
 function addItem(e) {
     e.preventDefault();
     newEl = document.createElement('li');
-    newText = document.createTextNode('New list item')
+    newText = document.createTextNode('New list item');
+    newEl.appendChild(newText);
+    elList.appendChild(newEl);
 }
+
+function updateCount() {
+    listItems = elList.getElementsByTagName('li');
+    newTex = document.createTextNode('New list item');
+    newEl.appendChild(newText);
+    elList.appendChild(newEl);
+}
+
+function updateCount() {
+    listItems = elList.getElementsByTagName('li').length;
+    counter.innerHTML = listItems;
+}
+
+addLink.addEventListener('click', addItem, false);
+elList.addEventListener('DOMNodeInserted', updateCount, false);
+
+// Adds list item(s) one at a time while updating the count
