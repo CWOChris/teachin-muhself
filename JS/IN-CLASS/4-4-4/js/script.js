a627f9988c9e0632edd8e242e56eec70305bcab9
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 01:45:18 -0500

    23sep23 first

--- JS/IN-CLASS/4-4-4/js/script.js
+++ JS/IN-CLASS/4-4-4/js/script.js
@@ -1,1 +1,8 @@
-var lettersEl = document.querySelector("#letters")
+var lettersEl = document.querySelector("#letters");
+var copy = "";
+document.addEventListener("keyup", function(e){
+    console.log(e.key);
+    console.log(e.code);
+    copy = copy + e.key;
+    lettersEl.textContent = copy;
+})
