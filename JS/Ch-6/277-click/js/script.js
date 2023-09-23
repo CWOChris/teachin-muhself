d1d61b74764f08cc8f1d0a4245cb9eeb1f18d06c
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 10:23:59 -0500

    make sure you capitalize where it says html in innerHTML

---
+++ JS/Ch-6/277-click/js/script.js
@@ -0,0 +1,18 @@
+var msg = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
+msg += '<div><h2>System Maintenance</h2>';
+msg += 'Our servers are being updated between 3 and 4 a.m. ';
+msg += 'During this time, there may be minor disruptions to service.</div>';
+
+var elNote = document.createElement('div');
+elNote.setAttribute('id', 'note');
+elNote.innerHTML = msg;
+document.body.appendChild(elNote);
+
+function dismissNote() {
+    document.body.removeChild(elNote);
+}
+
+var elClose = document.getElementById('close');
+elClose.addEventListener('click', dismissNote, false);
+
+
