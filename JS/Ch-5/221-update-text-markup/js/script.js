4510471d26de9b7f79c47e891a5c14aa08ea2f7e
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 07:20:45 -0500

    finished 221 successfully

---
+++ JS/Ch-5/221-update-text-markup/js/script.js
@@ -0,0 +1,7 @@
+var firstItem = document.getElementById('one');
+
+var itemContent = firstItem.innerHTML;
+
+firstItem.innerHTML = '<a href=\"http://example.org\">' + itemContent + '</a>';
+
+//works well
