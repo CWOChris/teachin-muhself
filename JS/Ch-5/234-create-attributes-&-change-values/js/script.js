2776325cbb99b1df3568af32318f4c86ef46c5ed
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 07:35:00 -0500

    234 finished, this is a neat and userful one

---
+++ JS/Ch-5/234-create-attributes-&-change-values/js/script.js
@@ -0,0 +1,7 @@
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+var fourthItem = document.getElementsByTagName('li').item(3);
+fourthItem.setAttribute('class', 'cool');
+
+// This one is pretty neat
