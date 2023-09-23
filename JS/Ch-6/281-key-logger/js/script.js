97f06b18af51d2c8903014b6d67c337fcdd36bf7
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 13:59:15 -0500

    281 finished

---
+++ JS/Ch-6/281-key-logger/js/script.js
@@ -0,0 +1,15 @@
+var el;
+
+function charCount(e) {
+    var textEntered, charDisplay, counter, lastkey;
+    textEntered = document.getElementById('message').value;
+    charDisplay = document.getElementById('charactersLeft');
+    counter = (180 - (textEntered.length));
+    charDisplay.textContent = counter;
+
+    lastkey = document.getElementById('lastkey');
+    lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode;
+}
+
+el = document.getElementById('message');
+el.addEventListener('keyup', charCount, false);
