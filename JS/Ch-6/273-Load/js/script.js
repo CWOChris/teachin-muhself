576c54389d1f5388e8473bbd0ee65d26223ed824
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-23 09:44:34 -0500

    273 finished

---
+++ JS/Ch-6/273-Load/js/script.js
@@ -0,0 +1,9 @@
+function setup() {
+var textInput;
+textInput = document.getElementById('username');
+textInput.focus();
+}
+
+window.addEventListener('load', setup, false);
+
+// this function will move the user's cursor into the text input box for username
