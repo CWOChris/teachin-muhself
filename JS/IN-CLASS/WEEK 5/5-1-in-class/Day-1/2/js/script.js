37b763f0569cffc4c3cba323db7fb82c3d182674
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-25 20:07:03 -0500

    local and session storage

---
+++ JS/IN-CLASS/WEEK 5/5-1-in-class/Day-1/2/js/script.js
@@ -0,0 +1,7 @@
+var firstNameEl = document.querySelector("#firstName");
+var lastNameEl = document.querySelector("#lastName");
+var submitEl = document.querySelector("#submit");
+
+submitEl.addEventListener("click", function(e){
+    
+})
