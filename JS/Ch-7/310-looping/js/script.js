d1eeb365303c23521255556326f19057139e2f5a
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-26 02:40:21 -0500

    finished 311

--- JS/Ch-7/310-looping/js/script.js
+++ JS/Ch-7/310-looping/js/script.js
@@ -1,2 +1,4 @@
 ('li em').addClass('seasonal');
 ('li.hot').addClass('favorite');
+
+// Doesn't work.  Not going to debug this one.
---
+++ JS/Ch-7/311-chaining/js/script.js
@@ -0,0 +1,3 @@
+$('li[id!="one"]').hide().delay(500).fadeIn(1400);
+
+//  All but first list item fade in over 1.4 sec.
