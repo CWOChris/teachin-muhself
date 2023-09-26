e26c20f1bdac3f0d9926570dc3fe71be5bf7b4bc
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-26 03:56:06 -0500

    333 also does not work

---
+++ JS/Ch-7/333-basic-effects/js/script.js
@@ -0,0 +1,12 @@
+// $(function() {
+//     $('h2').hide().slideDown();
+//     var $li = $('li');
+//     $li.hide().each(function(index) {
+//         $(this).delay(700 * index).fadeIn(700);
+//     });
+//     $li.on('click', fucntion() {
+//         $(this).fadeOut*(700);
+//     });
+// });
+
+// // doesn't function
