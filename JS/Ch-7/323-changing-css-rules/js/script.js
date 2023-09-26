826cf371a1129fbcf7309bb2d0a020e502f40cc4
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-26 03:23:20 -0500

    323 this one actually manipulates the style sheet.

---
+++ JS/Ch-7/323-changing-css-rules/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    var backgroundColor = $('li').css('background-color');
+    $('ul').append('<p>Color was: ' + backgroundColor + '</p>');
+    $('li').css({
+        'background-color': '#c5a996',
+        'border': '1px solid #fff',
+        'color': '#000',
+        'font-family': 'Georgia',
+        'padding-left': '+=75'
+    });
+});
+
+// Surprisingly, this works.
