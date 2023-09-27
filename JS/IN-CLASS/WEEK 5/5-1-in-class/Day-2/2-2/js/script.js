361a0818c4b734257ab9a560ceef0287b40a42a3
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-26 21:29:52 -0500

    2-2 is the one that you want to complete

--- JS/IN-CLASS/WEEK 5/5-1-in-class/Day-2/2-2/js/script.js
+++ JS/IN-CLASS/WEEK 5/5-1-in-class/Day-2/2-2/js/script.js
@@ -1,12 +1,44 @@
 $("#genresForm").on("submit", function(e){
     e.preventDefault();
-    var musicValue = $("input[name='music']").val(); //val() is .value in JS
-   // var musicValue = $("#musicID") //does the same thing as the code on line 3
-    console.log("musicValue", musicValue)
-    var musicItem = $("<li>");
-    musicItem.text(musicValue);
-    $("ul").append(musicItem);
-    $("input[name='music']").val("");
+    var musicValue = $
+})
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#genresForm").on("submit", function(e){
+//     e.preventDefault();
+//     var musicValue = $("input[name='music']").val(); //val() is .value in JS
+//    // var musicValue = $("#musicID") //does the same thing as the code on line 3
+//     console.log("musicValue", musicValue)
+//     var musicItem = $("<li>");
+//     musicItem.text(musicValue);
+//     musicItem.attr("data-genre", musicValue.toLowerCase());
+//     $("ul").append(musicItem);
+//     $("input[name='music']").val("");
+
+
+// });
---
+++ JS/IN-CLASS/WEEK 5/5-1-in-class/Day-2/2-3/js/script.js
@@ -0,0 +1,2 @@
+$("div").children().css("background-color: red");
+$("div").children().eq(1).attr("style", "border:1px solid blue");
