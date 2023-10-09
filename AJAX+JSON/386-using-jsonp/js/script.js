95028265684b8616f73f838d74cee4b2745087c0
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-10-09 02:47:33 -0500

    389 completed

---
+++ AJAX+JSON/386-using-jsonp/js/script.js
@@ -0,0 +1,12 @@
+function showEvents(data) {
+    var newContent = '';
+    for (var i = 0; i < data.events.length; i++) {
+        newContent += '<div class="event">';
+        newContent += '<img src="' + data.events[i].map + '"';
+        newContent += 'alt="' + data.events[i].location + '" />';
+        newContent += '<p><b>' + data.events[i].location + '</b><br>';
+        newContent += data.events[i].date + '</p>';
+        newContent += '</div>';
+    }
+    document.getElementById('content').innerHTML = newContent;
+}
